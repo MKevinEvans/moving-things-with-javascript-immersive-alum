906184b0af9ca0328a4d07cb0e298323c2ad2136
Kevin Evans <mkevinevans@gmail.com>
2019-10-15 17:22:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const app = "I don't do much."
-
+function load(){
 let dodger = document.getElementById('dodger');
 dodger.style.backgroundcolor="#000000"
+}
+document.addEventListener("DOMElements")
