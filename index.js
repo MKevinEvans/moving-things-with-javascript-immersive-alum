eb1da3263f19d36f3a4ea597d9327bf01c14b6b4
Kevin Evans <mkevinevans@gmail.com>
2019-10-15 17:25:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = "I don't do much."
 function load(){
   console.log("loaded")
-let dodger = document.getElementById('dodger');
-dodger.style.backgroundcolor="#000000"
+  let dodger = document.getElementById('dodger');
+  dodger.style.backgroundcolor="#000000"
 }
 document.addEventListener("DOMComtentLoaded", load);
